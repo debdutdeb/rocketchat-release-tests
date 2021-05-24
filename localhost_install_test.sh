e5bf4b5116a1d2e3740ca9c760e092d8f3847b72
r0zbot <r0zbot@users.noreply.github.com>
2021-05-24 19:26:29 -0300

    remove redundant test

--- localhost_install_test.sh
+++ localhost_install_test.sh
@@ -12,12 +12,4 @@ sleep 5
 echo "Running tests on rocketchat"
 ./basic_test.sh http://127.0.0.1:3000
 
-if [[ "$1" == "update" ]]; then
-  echo "Running another test"
-  ./basic_test.sh http://127.0.0.1:3000 
-  
-  echo "Seeing if information persisted across updates"
-  ./basic_test.sh http://127.0.0.1:3000 
-fi
-
 echo "Tests passed!"
