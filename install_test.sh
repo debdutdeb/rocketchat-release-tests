bc5c70de531c2737c090d3dad7c780e0d455edcf
r0zbot <r0zbot@users.noreply.github.com>
2021-05-24 21:22:08 -0300

    read ip addr from cli

--- install_test.sh
+++ install_test.sh
@@ -6,13 +6,10 @@ set -e
 cd "$(dirname "$0")"
 
 echo "Waiting for local rocket.chat server to start"
-./wait_http.sh "http://$TEST_IP:3000"
+./wait_http.sh "http://$1:3000"
 sleep 5
 
 echo "Running tests on rocketchat"
-./basic_test.sh "http://$TEST_IP:3000"
-
-echo "Running tests on rocketchat through proxy"
-./basic_test.sh https://$TEST_IP insecure
+./basic_test.sh "http://$1:3000"
 
 echo "Tests passed!"
